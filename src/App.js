bad23c4f7ee91f2846e25a97135d62f7820cd43c
zloyleva <zloyleva@gmail.com>
2019-04-13 12:34:30 +0300

    add new tasks

--- src/App.js
+++ src/App.js
@@ -1,14 +1,47 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
+import { connect } from "react-redux";
 
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.inputTaskRef = React.createRef();
+  }
+
+  render() {
+    const {tasks} = this.props;
+    return (
+      <div>
+        <form action="" onSubmit={this.onSubmitAddTask.bind(this)}>
+          <input type="text" ref={this.inputTaskRef}/>
+          <button>Add task</button>
+        </form>
+
+        <div>
+          <ul>
+            {
+              tasks.map((el,i) => (
+                  <li key={i}>{ el }</li>
+              ))
+            }
+          </ul>
+        </div>
       </div>
     );
   }
+
+  onSubmitAddTask(e){
+    e.preventDefault();
+    this.props.onAddTask(this.inputTaskRef.current.value);
+    this.inputTaskRef.current.value = "";
+  }
 }
 
-export default App;
+export default connect(
+    state => ({tasks: state}),
+    dispatch => ({
+      onAddTask(title){dispatch({type:"ADD_TASK", payload:title})}
+    })
+)(App);
--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
-const initState = ['something'];
+const initState = ['first task', 'second task'];
 const reducer = (state=initState,action) => {
     console.log(action);
-    if(action.type === "CHANGE_STORE"){
+    if(action.type === "ADD_TASK"){
         return [
             ...state,
             action.payload
@@ -20,17 +21,21 @@ const reducer = (state=initState,action) => {
 
 const store = createStore(reducer);
 
-console.log(store);
-console.log(store.getState());
+// console.log(store);
+// console.log(store.getState());
+//
+// store.subscribe(()=>{
+//     console.log("Change store",store.getState());
+// });
+//
+// store.dispatch({type:"CHANGE_STORE", payload: "something too"});
+// store.dispatch({type:"CHANGE_STORE", payload: "something other"});
 
-store.subscribe(()=>{
-    console.log("Change store",store.getState());
-});
-
-store.dispatch({type:"CHANGE_STORE", payload: "something too"});
-store.dispatch({type:"CHANGE_STORE", payload: "something other"});
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((
+    <Provider store={store}>
+        <App />
+    </Provider>
+), document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
